a7f1c47f63088c7c04a2f23b6757deecfff2342f
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-04-08 10:02:05 +0100

    Update print_bank_statement_spec.rb

--- spec/features/print_bank_statement_spec.rb
+++ spec/features/print_bank_statement_spec.rb
@@ -2,10 +2,10 @@ require 'bank_account.rb'
 
 describe 'Feature test: acceptance criteria' do
   it 'client can make a deposit, a withdrawal and print a bank statement' do
-    @bank_account = BankAccount.new
-    @bank_account.make_a_deposit(1000, '10-01-2012')
-    @bank_account.make_a_deposit(2000, '13-01-2012')
-    @bank_account.make_a_withdrawal(500, '14-01-2012')
+    bank_account = BankAccount.new
+    bank_account.make_a_deposit(1000, '10-01-2012')
+    bank_account.make_a_deposit(2000, '13-01-2012')
+    bank_account.make_a_withdrawal(500, '14-01-2012')
     expect { Printer.new.print_statement(@bank_account.transactions) }
       .to output(
         'date || credit || debit || balance
